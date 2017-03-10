8adb1f274496ca29422f9da5c7338f72f3e012fc
blackkitty <xiaomaozaier@msn.cn>
2017-03-10 23:34:38 +0800

    null

--- jsoner/jsoner.cpp
+++ jsoner/jsoner.cpp
@@ -15,12 +15,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define C_STRING_TYPE '"'
 #define C_NUMBER_TYPE '#'
 #define C_BOOL_TYPE '@'
+#define C_NULL_TYPE '$'
 
 int json2cmd(const char * jsonStrPtr, FILE * hSaveFile);
 bool _json2cmd(const char *&jsonStrPtr, FILE * hSaveFile, char * name);
-void getContent(const char * strPtr, char * bufferPtr);
+char * getContent(const char * strPtr);
 void ignoreBlank(char *& strPtr);
 
 #define MATCHED(c,s) (strchr(s,c)!=NULL)
@@ -33,17 +35,30 @@ inline bool IS_KEYCHAR(char c){
 }
 
 inline char TYPE_CHECK(const char * str){
+    if(*str == C_STRING_TYPE) 
+        return C_STRING_TYPE;
     if(*str >= '0' && *str <= '9')
         return C_NUMBER_TYPE;
     if(
         strstr(str,"true") == str && (IS_KEYCHAR(str[4]) || IS_WHITESPACE(str[4]))
       ||strstr(str,"false") == str && (IS_KEYCHAR(str[5]) || IS_WHITESPACE(str[5]))
       )
-      return C_BOOL_TYPE;
+        return C_BOOL_TYPE;
+    if(
+        strstr(str,"null") == str && (IS_KEYCHAR(str[4]) || IS_WHITESPACE(str[4]))
+    )
+        return C_NULL_TYPE;
     return *str;
 }
 
 int main(int argc, char *argv[]) {
+    /************test**************/
+    char *sl[] = {"123,","true,","null]","\"asdf sa\"","false "};
+    for(int i=0;i<sizeof(sl)/sizeof(char*);i++){
+        puts(getContent(sl[i]));
+    }
+    return 0;
+    /******************************/
 
     if(argc > 2){
 
@@ -70,7 +85,7 @@ inline void ignoreBlank(const char *& strPtr) {
     summary:
         get the content of key or value.
 */
-void getContent(const char * strPtr, char * bufferPtr){
+char * getContent(const char * strPtr){
     /*
         start   end         ignoreBlank
         "       "           0
@@ -78,21 +93,26 @@ void getContent(const char * strPtr, char * bufferPtr){
         [       ,]          1
         ,       ,]          1
         number  whitespace  0
-
-
     */
-    const char * endflag;
-    switch(TYPE_CHECK(*strPtr)){
-        case '"':           endflag = "\"";break;
-        case ':':           endflag = ",}";break;
-        case '[':           /* fallthrough... */
-        case ',':           endflag = ",]";break;
-        case C_BOOL_TYPE:   /* fallthrough... */
-        case C_NUMBER_TYPE: endflag = " \t\n{}[],:\0";break;
+    int ctSize;
+    char * buffer;
+    char ctType;
+    char * endFlag;
+
+    switch(ctType = TYPE_CHECK(strPtr)){
+        case C_STRING_TYPE:     endFlag = "\""; strPtr++;break;
+        case C_BOOL_TYPE:       /* fallthrough... */
+        case C_NULL_TYPE:       /* fallthrough... */
+        case C_NUMBER_TYPE:     endFlag = " \t\n{}[],:\0";break;
+        default: return NULL;
     }
+    printf("type: %c  ",ctType);
+    for(ctSize=0;!MATCHED(strPtr[ctSize], endFlag);ctSize++);
+    buffer = (char *)malloc(sizeof(char)*(ctSize+1));
+    memcpy(buffer, strPtr, sizeof(char)*ctSize);
+    buffer[ctSize] == '\0';
 
-    
-
+    return buffer;
 }
 
 int json2cmd(const char * jsonStrPtr, FILE * hSaveFile) {
@@ -101,10 +121,14 @@ int json2cmd(const char * jsonStrPtr, FILE * hSaveFile) {
     char elementName[256];
     p = jsonStrPtr;
     ignoreBlank(p);
-    for (; *p;) {
+    for (; *p;) { 
         if (*p == '"')
             _json2cmd(p, hSaveFile, elementName);
 
 
     }
+}
+
+bool _json2cmd(const char *&jsonStrPtr, FILE * hSaveFile, char * name){
+    return false;
 }
