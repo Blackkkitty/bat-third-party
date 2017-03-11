e95a2ad90d2979c99691a03f6bcd47a20b1e3b48
blackkitty <xiaomaozaier@msn.cn>
2017-03-11 14:09:28 +0800

    json2cmd

--- jsoner/jsoner.cpp
+++ jsoner/jsoner.cpp
@@ -1,7 +1,7 @@
 /*
     jsoner
     Version     : 0.1 beta
-    Auther      : Blackkitty
+    Auther      : blackkitty
     Date        : 2017-3-7
     Description : JSON parsing for batch
 
@@ -15,114 +15,95 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* content type */
 #define C_STRING_TYPE '"'
 #define C_NUMBER_TYPE '#'
 #define C_BOOL_TYPE '@'
 #define C_NULL_TYPE '$'
 #define C_BAD_TYPE '!'
 
-int json2cmd(const char * jsonStr, FILE * hSaveFile);
+#define MATCHED(c,s) (strchr(s,c)!=NULL)
+#define IS_WHITESPACE(c) (MATCHED(c," \t\n"))
+#define IS_KEYCHAR(c) (MATCHED(c,"{}[],:\0"))
+
+bool json2cmd(char * jsonStr, FILE * hSaveFile, char * eName);
 bool _json2cmd(const char *&jsonStr, FILE * hSaveFile, char * name);
 char * getContent(const char * str, int & ctSize);
 bool isNumber(const char *str);
 void whitespaceCLR(char *str);
+int itoa(int x, char *buffer);
+bool isNumber(const char *str);
+bool matchStr(const char *str1, const char *str2);
+char TYPE_CHECK(const char *str);
+char * getIndex(int inx);
 
-#define MATCHED(c,s) (strchr(s,c)!=NULL)
+class ValueName{
+public:
+    char key[4096];
 
-inline bool IS_WHITESPACE(char c){
-    return MATCHED(c," \t\n");
-}
-inline bool IS_KEYCHAR(char c){
-    return MATCHED(c,"{}[],:\0");
-}
-int itoa(int x, char * buffer){
-    if(x == 0){
-        *buffer = '0';
-        return 1;
-    }
-    int len = 0;
-    for(int z=x;z;z/=10) len++;
-    for(int i=len;x;x/=10){
-        buffer[--i] = '0' + x%10;
-    }
-    return len;
-}
-
-bool isNumber(const char *str){
-    /* -0.0e+003 */
-    bool ef, df, ff, fnf, pzf;
-    ef = false;     /* e flag */
-    df = false;     /* dot flag */
-    ff = true;      /* first position flag */
-    fnf = true;     /* first number flag */
-    pzf = false;    /* prefix zero flag */
-    for (const char *p = str; !IS_KEYCHAR(*p);p++){
-        if(MATCHED(*p, "+-")){
-            if(!ef && *p == '+') return false;
-            if(!ff) return false;
-            ff = false;
-        }
-        else if(MATCHED(*p, "eE")){
-            if(ff || fnf || ef) return false;
-            ff = fnf = ef = df = true;
-        }
-        else if(*p == '.'){
-            if(df || ff || fnf || ef) return false;
-            df = fnf = true;
-        }
-        else if(*p >= '0' && *p <= '9'){
-            if(*p == '0' && !df){
-                if(pzf) return false;
-                pzf = true;
-            }
-            ff = fnf = false;
-        }
-        else{
-            return false;
-        }
-    }
-    return !fnf;
-}
-bool matchStr(const char * str1, const char * str2){
-    for (; str2 != '\0';str1++,str2++)
-        if(*str1 != *str2)
-            return false;
-    return true;
-}
-
-inline char TYPE_CHECK(const char * str){
-    if(*str == '"'){
-        for (str++; *str != '"';str++);
-        if(IS_KEYCHAR(str[1]))
-            return C_STRING_TYPE;
-        else
-            return C_BAD_TYPE;
+    ValueName(){
+        stk = _stk;
+        *stk = 0;
+        memset(key,0,sizeof(key));
     }
 
-    if(matchStr(str, "true") || matchStr(str, "false")))
-        return C_BOOL_TYPE;
+    ValueName operator+=(const char * str){
+        int t = 0;
+        tail = key+*stk;
+        for(;*str != '\0';t++)
+            *tail++ = *str++;
+        *tail = '\0';
+        stk[1] = *stk + t;
+        stk++;
+        return *this;
+    }
     
-    if(matchStr(str, "null"))
-        return C_NULL_TYPE;
-    
-    if(isNumber(str))
-        return C_NUMBER_TYPE;
-    
-    return C_BAD_TYPE;
-}
+    ValueName back(){
+        stk--;
+        key[*stk] = '\0';
+        return *this;
+    }
+private:
+    int _stk[4096];
+    int *stk;
+    char *tail;
+};
 
 int main(int argc, char *argv[]) {
     /************test**************/
-    char s[120];
-    for (;~scanf("%s",s);){
-        printf("%s\n", isNumber(s) ? "true" : "false");
-    }
+    // char s[123];
+    // for(;~scanf("%s",s);){
+    //     puts(isNumber(s)?"true":"false");
+    // } 
+char json[] = "{\n\
+        \"id\":10086,\n\
+        \"name\":\"fuck\",\n\
+        \"children\":[\n\
+            {\n\
+                \"id\":12,\n\
+                \"name\":\"aaa\",\n\
+                \"friends\":[\n\
+                    \"tom\",\"jerry\",\"jack\"\n\
+                    [\"aa\",123,true,[false]]\n\
+                ]\n\
+            },\n\
+            {\n\
+                \"id\":2,\n\
+                \"name\":\"bbb\",\n\
+                \"goodman\":true\n\
+            }\n\
+        ],\n\
+        \"good man\":false,\n\
+        \"next\":null\n\
+    }";
+    puts(json);
+    json2cmd(json,NULL,"json");
     return 0;
     /******************************/
 
     if(argc > 2){
 
-
+        
         return 0;
     }
 
@@ -131,10 +112,107 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+
+
+/*
+    json2cmd
+    jsont to cmd main loop
+*/
+bool json2cmd(char * jsonStr, FILE * hSaveFile, char * eName) {
+
+    const char * p;
+    
+    char _stk[4096];            /* record "" {} [] : */
+    char *stk = _stk;
+
+    ValueName vn;             /* record element name */
+    vn += (const char *)eName;
+
+    int _inx[4096] = {0};       /* record list index */
+    int *inx = _inx;
+
+    whitespaceCLR(jsonStr);
+    p = jsonStr;
+    char * tmp;
+
+    for (; *p;p++) { 
+        //printf("    *p      : %c\n",*p);
+        //printf("    stk top : %c\n",*stk);
+        switch(*p){
+            case '{':
+                if(*stk == '{') return false;
+                *++stk = *p;
+                break;
+            case '[':
+                if(*stk == '{') return false;
+                *++stk = *p;
+                *++inx = 0;
+                tmp = getIndex(*inx);
+                vn += (const char *)tmp;
+                free(tmp);
+                break;
+            case '}':
+                if(*stk == '{') stk--;
+                else if(*stk == ':'){
+                    stk--,p--;
+                    vn.back();
+                    vn.back();
+                } 
+                else return false;
+                break;
+            case ']':
+                if(*stk == '[') stk--;
+                else return false;
+                inx--;
+                vn.back();
+                break;
+            case ':':
+                *++stk = *p;
+                break;
+            case ',':
+                if(*stk == '['){
+                    ++*inx;
+                    vn.back();
+                    tmp = getIndex(*inx);
+                    vn += (const char *)tmp;
+                    free(tmp);
+                }
+                if(*stk == ':'){
+                    stk--;
+                    vn.back();
+                    vn.back();
+                }
+                break;
+            default:
+                int ctSize = -1;
+                tmp = getContent(p, ctSize);
+                //printf("tmp: %s\n",tmp);
+                if(tmp == NULL) return false;
+                if(*stk == '{'){
+                    /* left value */
+                    vn += ".";
+                    vn += (const char *)tmp;
+                }
+                else{
+                    /* right value */
+                    printf("%s=%s\n",vn.key, tmp);
+                }
+                free(tmp);
+                //printf("ctSize:%d\n",ctSize);
+                p+=ctSize;
+                break;
+        }
+    }
+}
+
+bool _json2cmd(const char *&jsonStr, FILE * hSaveFile, char * name){
+    return false;
+}
+
+
 /*
     whitespaceCLR
-    summary:
-        delete all the whitespace who doesn't effect the value.
+    delete all the whitespace who doesn't effect the value.
 */
 void whitespaceCLR(char * str){
     bool flag = false;
@@ -150,15 +228,135 @@ void whitespaceCLR(char * str){
 
 
 /*
+    itoa
+    int to string.
+    Return length of string.
+
+    Attention:
+        enough memory of buffer.
+*/
+int itoa(int x, char * buffer){
+    if(x == 0){
+        *buffer = '0';
+        return 1;
+    }
+    int len = 0;
+    for(int z=x;z;z/=10) len++;
+    for(int i=len;x;x/=10){
+        buffer[--i] = '0' + x%10;
+    }
+    return len;
+}
+
+
+/*
+    getIndex
+    int to index string
+
+    Attention:
+        free the memory of index string.
+*/
+char * getIndex(int inx){
+    if(inx < 0) return NULL;
+    char * ret = (char *)malloc(sizeof(char)*15);
+    ret[0] = '[';
+    int t = itoa(inx, ret+1);
+    ret[t+1] = ']';
+    ret[t+2] = '\0';
+    return ret;
+} 
+
+/*
+    isNumber
+    judge if a string is a json number.
+*/
+bool isNumber(const char *str){
+    /* -0.0e+003 */
+    bool ef, df, ff, fnf, pzf, pnf;
+    ef = false;     /* e flag */
+    df = false;     /* dot flag */
+    ff = true;      /* first position flag */
+    fnf = true;     /* first number flag */
+    pzf = false;    /* prefix zero flag */
+    pnf = false;      /* prefix non-zero flag */
+    for (const char *p = str; !IS_KEYCHAR(*p);p++){
+        if(MATCHED(*p, "+-")){
+            if(!ef && *p == '+') return false;
+            if(!ff) return false;
+            ff = false;
+        }
+        else if(MATCHED(*p, "eE")){
+            if(ff || fnf || ef) return false;
+            ff = fnf = ef = df = true;
+            pnf = false;
+        }
+        else if(*p == '.'){
+            if(df || ff || fnf || ef) return false;
+            df = fnf = true;
+        }
+        else if(*p >= '0' && *p <= '9'){
+            if(*p == '0' && !pnf &&!df){
+                if(pzf) return false;
+                pzf = true;
+            }
+            if(*p != '0')
+                pnf = true;
+            ff = fnf = false;
+        }
+        else{
+            return false;
+        }
+    }
+    return !fnf;
+}
+
+/*
+    matchStr
+    judge if str2 matched str1. 
+*/
+bool matchStr(const char * str1, const char * str2){
+    for (; *str2 != '\0';str1++,str2++)
+        if(*str1 != *str2)
+            return false;
+    return IS_KEYCHAR(*str1);
+}
+
+/*
+    TYPE_CHECK
+    get the type of content.
+*/
+char TYPE_CHECK(const char * str){
+
+    if(matchStr(str, "true") || matchStr(str, "false"))
+        return C_BOOL_TYPE;
+    
+    if(matchStr(str, "null"))
+        return C_NULL_TYPE;
+    
+    if(isNumber(str))
+        return C_NUMBER_TYPE;
+
+    if(*str == '"'){
+        for (str++; *str != '"';str++);
+        if(IS_KEYCHAR(str[1]))
+            return C_STRING_TYPE;
+    }
+
+    return C_BAD_TYPE;
+}
+
+/*
     getContent
-    summary:
-        Return the content of key or value.
-        Return NULL if content do not comply with grammar.
+    Return content and size of content.
+    Return NULL if content do not comply with grammar.
+
+    Attention:
+        free the memory of content
 */
 char * getContent(const char * str, int & ctSize){
     char * buffer;      /*constent buffer*/
     char ctType;        /*content type*/
-    char * endFlag;
+    const char * endFlag;
 
     switch(ctType = TYPE_CHECK(str)){
         case C_STRING_TYPE:     endFlag = "\""; str++; break;
@@ -174,69 +372,10 @@ char * getContent(const char * str, int & ctSize){
     buffer = (char *)malloc(sizeof(char)*(ctSize+1));
     memcpy(buffer, str, sizeof(char)*ctSize);
     buffer[ctSize] = '\0';
+    if(ctType == C_STRING_TYPE)
+        ctSize++;
+    else
+        ctSize--;
 
     return buffer;
-}
-
-
-/*
-    json2cmd
-    summary:
-        jsont to cmd main loop
-*/
-int json2cmd(char * jsonStr, FILE * hSaveFile, char * eName) {
-
-    const char * p;
-    
-    char _stk[4096];            /* record "" {} [] : */
-    char *stk = _stk;
-
-    char name[4096];           /* record element name */
-    strcpy(name, eName);
-    char *nametail = name + strlen(name);
-    char *lasttail = name;
-
-    int _inx[4096] = {0};       /* record list index */
-    int *inx = _inx;
-
-
-    whitespaceCLR(jsonStr);
-    p = jsonStr;
-
-    for (; *p;) { 
-        switch(*p){
-            case '"':
-                if(*stk == '"') stk--;
-                else *stk-- = *p;
-
-                break;
-            case '{':
-                *stk++ = *p;
-                break;
-            case '[':
-                *stk++ = *p;
-                break;
-            case '}':
-                if(*stk == '{') stk--;
-                else return 1;
-                break;
-            case ']':
-                if(*stk == '[') stk--;
-                else return 1;
-                break;
-            case ':':
-                *stk++ = *p;
-                break;
-            case ',':
-                if(*stk == '[') ++*inx;
-                
-
-                break;
-            default:;
-        }
-    }
-}
-
-bool _json2cmd(const char *&jsonStr, FILE * hSaveFile, char * name){
-    return false;
 }
